7586589566e8a4fe23955bcaed7849b0e416e691
Dan Jacoby <jacobydan01@gmail.com>
2024-03-13 11:12:33 -0400

    commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity){
+    if (activity !== undefined){
+        return `This Saturday, I want to ${activity}!`;
+    } else {
+        return `This Saturday, I want to roller-skate!`;
+    }
+    
+}
+
+function mondayWork(work){
+    if (work !== undefined){
+        return `This Monday, I will ${work}.`;
+    } else {
+        return `This Monday, I will go to the office.`;
+    }
+    
+}
+
+function wrapAdjective(wrap = "*"){
+    return function (adj = "special"){
+        return `You are ${wrap}${adj}${wrap}!`;
+    };
+}
+
